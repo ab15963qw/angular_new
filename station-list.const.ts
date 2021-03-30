1f059d8d7eff87cce736d77e851924c294430562
ab15963qw <36786222+ab15963qw@users.noreply.github.com>
2021-03-30 20:34:44 +0800

    Add files via upload

---
+++ station-list.const.ts
@@ -0,0 +1,15 @@
+export const stationList = [
+    {
+      'Station': '三重國小站',
+      'Destination': '南勢角站',
+      'UpdateTime': '2018-10-15T22:14:41.557'
+    },
+    /*
+     * 太長了，中間省略。
+     */
+    {
+      'Station': '麟光站',
+      'Destination': '南港展覽館站',
+      'UpdateTime': '2018-10-15T22:14:41'
+    }
+  ];
